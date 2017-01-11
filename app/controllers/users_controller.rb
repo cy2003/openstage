2be20579e9f5ce9247e1489176a69e15fa20dddd
Rob Hitt <robhitt@gmail.com>
2017-01-11 10:01:25 -0500

    performers beta

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,11 +6,12 @@ class UsersController < ApplicationController
 
   def new
     @user = User.new
+
   end
 
   def create
     binding.pry
-    @user = user.new(user_params)
+    @user = User.new(user_params)
     if password_exists && @user.save
       session[:user_id] = @user.id
       redirect_to user_path(@user)
@@ -24,9 +25,6 @@ class UsersController < ApplicationController
   end
 
   def show
-  end
-
-  def create
   end
 
   def destroy
