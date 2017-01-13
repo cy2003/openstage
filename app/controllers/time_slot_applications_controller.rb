9b22d4d3e70e1381e849091d1b02d9f5175e5e93
Valerie McCarthy <valeriewilsonmccarthy@gmail.com>
2017-01-13 09:39:43 -0500

    Night edits

--- app/controllers/time_slot_applications_controller.rb
+++ app/controllers/time_slot_applications_controller.rb
@@ -1,11 +1,19 @@
 class TimeSlotApplicationsController < ApplicationController
 
-  def new
+  def index
+    @@timeslotapplications = TimeSlotApplication.all
+  end
 
+  def new
+    @@timeslotapplication = TimeSlotApplication.new
   end
 
   def create
     @timeslotapplication = TimeSlotApplication.create(performer_id: params[:performer_id], time_slot_id: params[:time_slot_id], status: params[:status])
+    
+    @time_slot = TimeSlot.find(params[:time_slot_id])
+    @time_slot.status = params[:status]
+    @time_slot.save
     flash[:notice] = "Your request has been made."
     redirect_to user_path(current_user)
   end
